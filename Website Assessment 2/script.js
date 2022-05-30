698f10bb2816dd164824f2ab4e938be129036f47
Jazim hammad <90127535+jazim-hammad@users.noreply.github.com>
2022-05-30 17:28:14 +0400

    Web Dev Assessment 2
    website for assessment 2

---
+++ Website Assessment 2/script.js
@@ -0,0 +1,11 @@
+/*java script for home page text typing*/
+
+var typed = new Typed(".auto-type", {
+  /*texts to be typed*/
+  strings: ["PROFESSIONAL", "QUICK", "EFFICIENT"],
+  /*typing and backspace speed*/
+  typeSpeed: 180,
+  backSpeed: 180,
+  /*loops the text infinitely*/
+  loop: true,
+});
